86aa0dee931ec600888674b877f833131578b87b
Bhavya sri <123apuribhavyasri@gmail.com>
2025-10-10 12:03:22 +0530

    Update while.dart

--- Week-3/while.dart
+++ Week-3/while.dart
@@ -3,8 +3,10 @@ void main(){
   print("Enter a Number");
   int N=int.parse(stdin.readLineSync()!);
   int sum=0;
-  for(int i =N;i>0;i=(i/10).floor()){
+  int i=N;
+  while(i>0){
     sum+=(i%10);
+    i=(i/10).floor();
   }
   print('Sum of digits\n$sum');
   }
